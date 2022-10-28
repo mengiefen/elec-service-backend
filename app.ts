d13b603a0fb2054177bf528743c9fe4f66305403
mengiefen <mengiefen@gmail.com>
2022-10-28 22:13:38 +0300

    modify the app accordingly

--- app.ts
+++ app.ts
@@ -4,6 +4,12 @@ import dotenv from 'dotenv';
 import userRouter from './router/user';
 import morgan from 'morgan';
 import cors from 'cors';
+import equipmentRouter from './router/equipment';
+import expendableRouter from './router/expendable';
+import withdrawalRouter from './router/withdrawal';
+import projectRouter from './router/project';
+import storeRouter from './router/storage';
+import reservationRouter from './router/reservation';
 
 dotenv.config();
 
@@ -13,9 +19,9 @@ const port: number = 3000;
 const MONGO_URI: string =
   process.env.MONGO_URI || 'mongodb://localhost:27017/test';
 
-const allowedRegions = '*';
+const allowedOrigins = '*';
 const options: cors.CorsOptions = {
-  origin: allowedRegions,
+  origin: allowedOrigins,
 };
 
 // Middleware
@@ -35,6 +41,12 @@ mongoose
 
 // Routes
 app.use('/api/v1', userRouter);
+app.use('/api/v1', withdrawalRouter);
+app.use('/api/v1', projectRouter);
+app.use('/api/v1', storeRouter);
+app.use('/api/v1', equipmentRouter);
+app.use('/api/v1', expendableRouter);
+app.use('/api/v1', reservationRouter);
 
 // Start the server
 app.listen(port, host, () => {
